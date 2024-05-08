4e13a9b4845f64069c50c912e1c32e8dca2f07a4
Anderson-Lai <andersonmhlai26@gmail.com>
2024-05-08 16:41:08 -0400

    Add project files.

---
+++ File Editor/main.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#define _CRTDBG_MAP_ALLOC
+#include <crtdbg.h>
+
+#define nullptr NULL
+
+int main(int argc, char** argv)
+{
+    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+
+    
+
+
+
+    return 0;
+}
